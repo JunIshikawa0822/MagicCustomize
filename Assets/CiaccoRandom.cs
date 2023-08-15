6a8015200fcff7c24142402b2c967d69bcb2fcd9
JunIshikawa0822 <jun.ffcc83@gmail.com>
2023-08-15 23:20:33 +0900

    MagicCircle almost done

---
+++ Assets/CiaccoRandom.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class CiaccoRandom
+{
+    private static int tree = 0;
+
+    // set the seed
+    public void setSeed(int seed)
+    {
+        tree = Math.Abs(seed) % 9999999 + 1;
+        getRand(0, 9999999);
+    }
+
+    // get a random int between $min and $max [both included]
+    public int getRand(int min, int max)
+    {
+        Debug.Log("tree " + tree);
+        tree = (tree * 123) % 69522569;
+        Debug.Log("tree " + tree);
+        return tree % (max - min + 1) + min;
+    }
+}
